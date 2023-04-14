fcc21744f362980458321de16552a7779c330d7f
xxyeon129 <xxyeon129@gmail.com>
2023-04-14 12:03:19 +0900

    [feat] 투두 제목, 내용 입력 기능 구현, 로컬스토리지에 입력 내용 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import ToDoForm from "components/ToDoForm";
+import { useState } from "react";
+
 function App() {
-    return <div>To Do List</div>;
+    const getTodos = JSON.parse(localStorage.getItem("toDoData"));
+    const storageTodos = getTodos === null ? [] : getTodos;
+    const [todos, setTodos] = useState(storageTodos);
+
+    return (
+        <div>
+            To Do List
+            <ToDoForm todos={todos} setTodos={setTodos} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ToDoForm.js
@@ -0,0 +1,46 @@
+import { useState, useRef } from "react";
+
+export default function ToDoTemplate({ todos, setTodos }) {
+    const [inputTitle, setInputTitle] = useState("");
+    const [inputContent, setInputContent] = useState("");
+
+    let nextId = useRef(0);
+
+    const addTodo = (title, content) => {
+        nextId.current += 1;
+        const todo = { id: nextId.current, title, content, checked: false };
+        setTodos([...todos, todo]);
+        localStorage.setItem("toDoData", JSON.stringify([...todos, todo]));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (inputTitle === "") {
+            return alert("제목을 입력해주세요.");
+        }
+        if (inputContent === "") {
+            return alert("할 일을 입력해주세요.");
+        }
+        addTodo(inputTitle, inputContent);
+        setInputTitle("");
+        setInputContent("");
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>제목</label>
+                <input
+                    placeholder="할 일을 요약해서 입력해주세요."
+                    onChange={(e) => setInputTitle(e.target.value)}
+                />
+                <label>내용</label>
+                <input
+                    placeholder="할 일을 입력해주세요."
+                    onChange={(e) => setInputContent(e.target.value)}
+                />
+                <button type="submit">추가하기</button>
+            </form>
+        </>
+    );
+}
