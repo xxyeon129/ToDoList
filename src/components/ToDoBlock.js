cc2f305bc4cef35ca64fc69410d53b3e39860b80
xxyeon129 <xxyeon129@gmail.com>
2023-04-15 11:26:37 +0900

    [design] styled-components 설치, 전체 CSS 적용

--- src/components/ToDoBlock.js
+++ src/components/ToDoBlock.js
@@ -1,14 +1,52 @@
+import styled from "styled-components";
+
 export default function ToDoBlock({ todo, deleteTodo, doneTodo }) {
     return (
-        <span>
+        <BlockStyle>
             <h2>{todo.title}</h2>
             <p>{todo.content}</p>
-            <button onClick={() => deleteTodo(todo.id)}>삭제하기</button>
+            <button id="delete" onClick={() => deleteTodo(todo.id)}>
+                삭제하기
+            </button>
             {todo.isDone ? (
                 <button onClick={() => doneTodo(todo.id)}>취소</button>
             ) : (
                 <button onClick={() => doneTodo(todo.id)}>완료</button>
             )}
-        </span>
+        </BlockStyle>
     );
 }
+
+const BlockStyle = styled.div`
+    padding: 1rem 1.5rem;
+    width: 13rem;
+    border: 3px solid #070a52;
+    border-radius: 10px;
+
+    button {
+        width: 6rem;
+        padding: 0.5rem;
+        margin-bottom: 0.8rem;
+        background: inherit;
+        border: 2px solid #3c486b;
+        border-radius: 10px;
+        cursor: pointer;
+
+        :hover {
+            transition: 0.5s;
+            color: white;
+            background: #3c486b;
+        }
+    }
+
+    #delete {
+        margin-right: 0.5rem;
+        border: 2px solid #d21312;
+        border-radius: 10px;
+
+        :hover {
+            transition: 0.5s;
+            background: #d21312;
+        }
+    }
+`;
