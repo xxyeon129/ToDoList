4a844a20ca4d4f4526bf81753e8e0920b1515cef
xxyeon129 <xxyeon129@gmail.com>
2023-04-20 09:56:38 +0900

    [feat] To Do 상세페이지 기능 구현

--- src/pages/ToDoDetail.js
+++ src/pages/ToDoDetail.js
@@ -1,3 +1,19 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
 export default function ToDoDetail() {
-    return <>To Do Detail</>;
+    const todoData = useSelector((todosModule) => todosModule.todoReducer);
+    const param = useParams();
+
+    const todoDetail = todoData.find((todo) => todo.id === parseInt(param.id));
+    console.log(todoDetail);
+
+    return (
+        <>
+            <p>ID: {todoDetail.id}</p>
+            <h2>{todoDetail.title}</h2>
+            <div>{todoDetail.content}</div>
+            <button>이전으로</button>
+        </>
+    );
 }
